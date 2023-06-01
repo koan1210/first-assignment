9c809624e5c34d429b9dcedaac2424eb222420ee
koan <gusgh7729@gmail.com>
2023-06-01 22:44:46 +0900

    최종
    ㅈㅈ

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // 영화 API 구현
-// 1. fetch - movieAPI에서 데이터 불러오기(JSON 형태)
 const fetchMovieData = async () => {
   const fetchMovieOptions = {
     method: "GET",
@@ -36,3 +35,43 @@ const createMovieCards = async () => {
 };
 
 createMovieCards();
+
+// 영화 포스터 클릭
+const handleMoviePosterClick = (movieId) => {
+  alert(`선택한 영화의 ID: ${movieId}`);
+};
+
+// 영화 카드 클릭
+movieCardList.addEventListener("click", (event) => {
+  const movieCard = event.target.closest(".movie-card");
+  if (movieCard) {
+    const movieId = movieCard.id;
+    handleMoviePosterClick(movieId);
+  }
+});
+
+// 영화 검색
+const searchForm = document.getElementById("search-form");
+const searchInput = document.getElementById("search-input");
+
+searchForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const searchKeyword = searchInput.value.toLowerCase();
+
+  const movies = await fetchMovieData();
+  const filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(searchKeyword)
+  );
+  movieCardList.innerHTML = filteredMovies
+    .map(
+      (movie) =>
+        `<div class="movie-card" id=${movie.id}>
+            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="movie-poster" />
+            <h3 class="movie-title">${movie.title}</h3>
+            <p class="release-date">Release date : ${movie.release_date}</p>
+            <p class="vote-average">Rating : ${movie.vote_average}</p>
+            <p class="movie-overview">${movie.overview}</p>
+        </div>`
+    )
+    .join("");
+});
